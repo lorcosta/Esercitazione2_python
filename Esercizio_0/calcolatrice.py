3a45ce18da4488d15026d7d09b43d1952c013169
Lorenzo <costa.lorenzo98@gmail.com>
2020-04-27 19:38:52 +0200

    Esercizio 0, calcolatrice

--- Esercizio_0/calcolatrice.py
+++ Esercizio_0/calcolatrice.py
@@ -1,2 +1,7 @@
+from GestoreCalcoli import *
 if __name__ == '__main__':
-	print("vediamo se a sto giro va")
+	c=GestoreCalcoli()
+	print(f'La somma vale:{c.add(2,3)}')
+	print(f'La sottrazione vale: {c.sub(2,3)}')
+	print(f'La moltiplicazione vale: {c.mul(2,3)}')
+	print(f'La divisione vale: {c.div(2,3)}')
