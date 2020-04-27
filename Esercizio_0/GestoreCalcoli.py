a2944cca27cd831aff82c38c9967d87f8b7945a0
Lorenzo <costa.lorenzo98@gmail.com>
2020-04-27 20:04:25 +0200

    calcolatrice

---
+++ Esercizio_0/GestoreCalcoli.py
@@ -0,0 +1,9 @@
+class GestoreCalcoli:
+	def add(self,numero1,numero2):
+		return numero1+numero2
+	def sub(self,numero1,numero2):
+		return numero1-numero2
+	def mul(self,numero1,numero2):
+		return numero1*numero2
+	def div(self,numero1,numero2):
+		return numero1/numero2
